3d08015d5c4b10270a19ad0b42520a6a9b28afb1
JLuisusr <jl.studiositas@gmail.com>
2025-10-16 20:50:41 -0300

    up

--- World_1.py
+++ World_1.py
@@ -1,82 +1,40 @@
 listaExercicios = [
 'Digite de 1 a 115 para ver os exercícios ou 0 para encerrar\nQual Exercício executar? ',
-'Exercício 001: Crie um programa que escreva "Olá, mundo!" na tela.',
-'Exercício 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.',
-'Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.',
-'Exercício 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.',
-'Exercício 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.',
-'Exercício 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.',
-'Exercício 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.',
-'Exercício 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.',
-'Exercício 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.',
-'Exercício 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares nela pode comprar.',
-'Exercício 011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área \n e a quantidade de tinta necessária para pintá-la,\nsabendo que cada litro de tinta pinta uma área de 2 metros quadrados.',
-'Exercício 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.',
-'Exercício 013: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.',
-'Exercício 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.',
-'Exercício Python 15: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e \n a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.',
-'Exercício 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.',
-'Exercício 017: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.',
-'Exercício 018: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.'
-'Exercício 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro.\n Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.',
-'Exercício 020: O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.\nFaça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.',
-'Exercício 021: Faça um programa em que abra e reproduza o áudio de um arquivo MP3.',
-'Exercício 022: Crie um programa que leia o nome completo de uma pessoa e mostre:\n– O nome com todas as letras maiúsculas e minúsculas.\n– Quantas letras ao todo (sem considerar espaços).\n– Quantas letras tem o primeiro nome.',
-'Exercício 023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.',
-'Exercício 024: Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".',
-'Exercício 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.',
-'Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece \n a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.',
-'Exercício 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro\n e o último nome separadamente.',
-'Exercício 028: Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.',
-'Exercício 029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,\n mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.',
-'Exercício 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.',
-'Exercício 031: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.',
-'Exercício 032: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.',
-'Exercício 033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.',
-'Exercício 034: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.\nPara salários superiores a R$1250,00, calcule um aumento de 10%.\nPara os inferiores ou iguais, o aumento é de 15%.',
-'Exercício 035: Desenvolva um programa que leia o comprimento de três retas\n e diga ao usuário se elas podem ou não formar um triângulo.']
-
-Ex001 ='Exercício 001: Crie um programa que escreva "Olá, mundo!" na tela.'
-Ex002 = 'Exercício 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.'
-Ex003 = 'Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.'
-Ex004 = 'Exercício 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.'
-Ex005 = 'Exercício 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.'
-Ex006 = 'Exercício 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.'
-Ex007 = 'Exercício 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.'
-Ex008 = 'Exercício 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.'
-Ex009 = 'Exercício 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.'
-Ex010 = 'Exercício 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares'
-'nela pode comprar.'
-Ex011 = 'Exercício 011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área \n\
-e a quantidade de tinta necessária para pintá-la,\nsabendo que cada litro de tinta pinta uma área de 2 metros quadrados.'
-Ex012 = 'Exercício 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.'
-Ex013 = 'Exercício 013: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.'
-Ex014 = 'Exercício 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.'
-Ex015 = 'Exercício Python 15: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e \n a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.'
-Ex016 = 'Exercício 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.'
-Ex017 = 'Exercício 017: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.'
-Ex018 = "Exercício 018: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo."
-Ex019 = 'Exercício 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro.\nFaça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.'
-Ex020 = 'Exercício 020: O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.\nFaça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'
-Ex021 = 'Exercício 021: Faça um programa em que abra e reproduza o áudio de um arquivo MP3.'
-Ex022 = 'Exercício 022: Crie um programa que leia o nome completo de uma pessoa e mostre:\n– O nome com todas as letras maiúsculas e minúsculas.\n– Quantas letras ao todo (sem considerar espaços).\n– Quantas letras tem o primeiro nome.'
-Ex023 = 'Exercício 023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.'
-Ex024 = 'Exercício 024: Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".'
-Ex025 = 'Exercício 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'
-Ex026 = 'Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece \n a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'
-Ex027 = 'Exercício 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro\n '
-'e o último nome separadamente.'
-Ex028 = 'Exercício 028: Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.'
-Ex029 = 'Exercício 029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,\n\
-" mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.'
-Ex030 = 'Exercício 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.'
-Ex031 = "Exercício 031: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas"
-Ex032 = 'Exercício 032: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.'
-Ex033 = 'Exercício 033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.'
-Ex034 = 'Exercício 034: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.\nPara salários superiores a R$1250,00, calcule um aumento de 10%.\nPara os inferiores ou iguais, o aumento é de 15%.'
-Ex035 = "Exercício 035: Desenvolva um programa que leia o comprimento de três retas\ne diga ao usuário se elas podem ou não formar um triângulo."
-
-
+'Exercício Python  001: Crie um programa que escreva "Olá, mundo!" na tela.',
+'Exercício Python  002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.',
+'Exercício Python  003: Crie um programa que leia dois números e mostre a soma entre eles.',
+'Exercício Python  004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.',
+'Exercício Python  005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.',
+'Exercício Python  006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.',
+'Exercício Python  007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.',
+'Exercício Python  008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.',
+'Exercício Python  009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.',
+'Exercício Python  010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares nela pode comprar.',
+'Exercício Python  011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área \n e a quantidade de tinta necessária para pintá-la,\nsabendo que cada litro de tinta pinta uma área de 2 metros quadrados.',
+'Exercício Python  012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto".',
+'Exercício Python  013: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.',
+'Exercício Python  014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.',
+'Exercício Python  015: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e \n a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.',
+'Exercício Python  016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.',
+'Exercício Python  017: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.',
+'Exercício Python  018: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.'
+'Exercício Python  019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro.\n Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.',
+'Exercício Python  020: O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.\nFaça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.',
+'Exercício Python  021: Faça um programa em que abra e reproduza o áudio de um arquivo MP3.',
+'Exercício Python  022: Crie um programa que leia o nome completo de uma pessoa e mostre:\n– O nome com todas as letras maiúsculas e minúsculas.\n– Quantas letras ao todo (sem considerar espaços).\n– Quantas letras tem o primeiro nome.',
+'Exercício Python  023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.',
+'Exercício Python  024: Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".',
+'Exercício Python  025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.',
+'Exercício Python  026: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece \n a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.',
+'Exercício Python  027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro\n e o último nome separadamente.',
+'Exercício Python  028: Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.',
+'Exercício Python  029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,\n mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.',
+'Exercício Python  030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.',
+'Exercício Python  031: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.',
+'Exercício Python  032: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.',
+'Exercício Python  033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.',
+'Exercício Python  034: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.\nPara salários superiores a R$1250,00, calcule um aumento de 10%.\nPara os inferiores ou iguais, o aumento é de 15%.',
+'Exercício Python  035: Desenvolva um programa que leia o comprimento de três retas\n e diga ao usuário se elas podem ou não formar um triângulo.']
 #--------------------------------------------------------------------------------------------------------------------------------------
 # EXERCISES ONE WORLD
 #---------------------------------------------------------------------------------------------------------------------------------------#
