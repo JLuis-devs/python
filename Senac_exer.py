b70f2eb8bd2f2ee0adcb381de1b28b666af51ac8
JLuisusr <luis.work0124@gmail.com>
2025-10-21 08:03:20 -0300

    up

---
+++ Senac_exer.py
@@ -0,0 +1,28 @@
+
+'''
+ C = K - 273.15 ou K = C - 273.15 
+ C = 5(f - 32)/9 ou f = (9/5)C +32 
+ f = 9(K/5 - 273/5 + 32)
+
+'''
+
+print('Calculadora de temperatura: \n Opções:\n     [1] - Celsius\n     [2] - Fahrenheit\n     [3] - Kelvin')
+print('Exemplo: para calcular de Celsius para Kelvin digite opção 1 enter opção 3.')
+escalaInicial = input('Deseja converter de: ')
+escalaFinal = input('Deseja converter de: ')
+
+conversao = int(escalaInicial + escalaFinal)
+match conversao:
+    case 12:
+        
+    case 12:
+        SDSDS
+    case 13:
+        SDSDS
+    case 21:
+    case 22:
+    case 23:
+    case 31:
+    case 32:
+    case 33:
+
